e11cbbfbe460b884386c9d17aa632341245506cb
lailahgrant <lailahgrant19@gmail.com>
2021-04-19 17:52:31 +0300

    implementing the js logic to display all products

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ let cart = [];
 class Products{
 //get all the products dynamically
     
+    //create methods
     //method to get json with products
     // getProducts() {
     //     //use fetch
@@ -32,8 +33,27 @@ class Products{
     async getProducts() {
         //put our await code in a try block
         try {
-            let result = await fetch('data.json');
-            return result;
+            let result = await fetch("data.json"); //use asynchronize way
+            //return data using the json method on fetch
+            let data = await result.json();    //here we're getting our data in the json format
+            
+            //return result;  instead of this initial return, we return data
+
+            //return data; destructure this more by doing the following
+            let products = data.items; //items is from the json - array
+            //use map since it is an array
+            products = products.map(item => {
+                //for each array item, I destructure it
+                const { title, price } = item.fields; //still .fields from data.json
+                const { id } = item.sys;
+                const image = item.fields.image.fields.file.url;
+                //after this iteration , I return a clean object
+                return {title, price, id, image}
+            })
+            //return products;
+            return products;
+
+
         } catch (error) {
             console.log(error);
         }
@@ -42,9 +62,41 @@ class Products{
     
 }
 
+//display products
 //class for displaying products or the UI - responsible for grtting all the items returned from class Products and displaying them. 
 class UI{
+    //will have most of the methods
+    //everything that will be displayed on the screen will be here
+    displayProducts(products) {
+        //console
+        //console.log(products);
 
+        let result = ''; //empty string for now
+        //since an array is being got as an output, I loop throught it
+        products.forEach(product => { //this looping will display all the products(as per what is in the json)
+            //get properties from the object & place them in the html already have 
+            //use += inorder not to override but just add
+            //use template literals to access properties that will be on each array to be displayed.
+            result += `
+            <!-- single product -->
+             <article class="product__">
+                        <div class="img__container">
+                            <img class="product__img" src=${product.image} alt="Plate of salads">
+                            <button class="bag-btn" data-id= ${product.id}>
+                                <i class="bx bx-shopping-bag"></i>
+                                Add to bag
+                            </button>
+                        </div>
+                        <h3>${product.title}</h3>
+                        <h4>${product.price}</h4>
+                    </article>
+                    <!--End of  Single Product  -->
+            `
+        });
+        //
+        //insert the products into the productsDOM(from the variables above)
+        productsDOM.innerHTML = result;
+    }
 }
 
 //local storage
@@ -59,7 +111,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const ui = new UI(); //instance of the class UI
     const products = new Products();
-})
+
+    //get all products
+    //products.getProducts().then(data => console.log(data));
+    //use the products from the getProducts()
+    // products.getProducts().then(products => { console.log(products) });
+    
+    //need to display content on the screen (ui) & not console - then we use ui.displayProducts(call the function in the class UI)
+    products.getProducts().then(products => ui.displayProducts(products));
+
+
+});
 
 
 
